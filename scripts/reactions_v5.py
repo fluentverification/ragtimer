efac8985d101c75660944af13038ec8f655c2763
Landon Taylor <landon.jeffrey.taylor@usu.edu>
2022-09-14 15:15:16 -0600

    Oops, had wrong models copying

--- scripts/reactions_v5.py
+++ scripts/reactions_v5.py
@@ -79,7 +79,7 @@ def randTest(runswanted, reactions1, prefix, prefix_index, loose=False, printing
         for react in reactions:
             if react.priority == -1:
                 react.priority = newTierIfLoose
-                react.tierCount = 20
+                react.tierCount = 45
                 if printing:
                     print(react.reactants, react.priority)
 
